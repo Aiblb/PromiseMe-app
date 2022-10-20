fb5837d38255c95fda48fb3a339118e46c8246b9
adri-anaaa <114315254+adri-anaaa@users.noreply.github.com>
2022-10-20 07:44:23 -0600

    Agenda
    Hola.
    Hice la agnda al menos la to od list pero por alguna razón se borró el archivo que hiciste de primero y hice otro faltan cosas de la agenda pero aja ya está al menos una lista y no se como conectarlo con lo demás. Pero intenté :D

---
+++ resources/views/agenda.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+<div class="card">
+              <div class="card-header">
+              To do list
+              </div>
+              <div class="card-body">
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                  <label class="form-check-label" for="inlineCheckbox1">I promise i will rest more this wek</label>
+                </div>
+                <br>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                  <label class="form-check-label" for="inlineCheckbox2">I promise i will eat fruits today</label>
+                </div>
+              </div>
+            </div>
+</x-layout>
